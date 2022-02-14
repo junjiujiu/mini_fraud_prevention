a516578312e8e0e5f5f8367338d789b0145c26b7
junjiujiu <3066582731@qq.com>
2022-02-14 17:13:10 +0800

    first commit

---
+++ assets/js/network.js
@@ -0,0 +1,29 @@
+const fzpurl = "http://192.168.100.1:8037/fraud_prevention"
+var app = getApp()
+
+module.exports={
+  http:http,
+  hdpfurl:fzpurl,
+}
+// 网络请求
+function http(url, method, data, succ, fail, com) {
+  // 小程序顶部显示Loading
+  wx.request({
+    url: fzpurl + url,
+    method: method,
+    data: data,
+    header: {
+      'content-type': 'application/x-www-form-urlencoded',
+    },
+    // 成功后的回调
+    success: res => {
+      console.log(url + ' 返回的data:', res.data);
+      if (succ) succ(res);
+    },
+    fail: err => {
+      if (fail) fail(err);
+    },
+    complete: com => {
+    }
+  })
+}
